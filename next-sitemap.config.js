08d71a427370955f3f6f8fc18aaa17098bb4fc73
kobaraxmedical <controlpanelkobaraxmedical@outlook.com>
2025-02-25 07:58:07 +0000

    update SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "www.kobarax-medical.com", // Remplacez par l'URL de votre site
+    generateRobotsTxt: true, // Génère aussi un robots.txt
+    sitemapSize: 5000, // Taille maximale par fichier de sitemap
+  };
+  
--- pages/_app.js
+++ pages/_app.js
@@ -18,8 +18,35 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <title>KOBARAX MEDICAL Sarl - Une Technologie Médicale De Pointe, Pour Un Plateau Médical À La Hauteur.</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"></meta>
+      <title>KOBARAX MEDICAL Sarl - Une Technologie Médicale De Pointe</title>
+      <meta name="description" content="KOBARAX MEDICAL Sarl offre des solutions médicales de pointe pour un plateau médical à la hauteur de vos attentes." />
+      <meta name="keywords" content="médecine, technologie médicale, équipements médicaux, santé, innovation médicale" />
+      <meta name="author" content="KOBARAX MEDICAL Sarl" />
+      <meta property="og:title" content="KOBARAX MEDICAL Sarl - Technologie Médicale De Pointe" />
+      <meta property="og:description" content="Découvrez les solutions de KOBARAX MEDICAL pour un plateau médical avancé." />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="www.kobarax-medical.com" />
+      <meta property="og:image" content="www.kobarax-medical.com/images/about-1.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="KOBARAX MEDICAL Sarl" />
+      <meta name="twitter:description" content="Technologie médicale innovante pour améliorer les soins de santé." />
+      <meta name="twitter:image" content="www.kobarax-medical.com/images/about-1.png" />
+       {/* Balises JSON-LD pour améliorer le SEO */}
+       <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "MedicalBusiness",
+            "name": "KOBARAX MEDICAL Sarl",
+            "description": "KOBARAX MEDICAL Sarl, filiale de KOBARAX Co., Ltd. (Japon), est votre partenaire en Afrique de l'Ouest pour des équipements médicaux de qualité. Basés au Sénégal, nous produisons, vendons et assurons la maintenance de dispositifs fiables pour les professionnels de la santé. Notre gamme d'équipements, issue de fabricants de renom, est soutenue par une équipe d'ingénieurs et de techniciens cumulant plus de 30 ans d'expertise. Nous garantissons un service réactif et efficace, permettant à nos clients de se concentrer sur leurs patients.",
+            "url": "www.kobarax-medical.com",
+            "image": "www.kobarax-medical.com/images/about-1.png",
+            "contactPoint": {
+              "@type": "ContactPoint",
+              "telephone": "+221-32-824-64-86",
+              "contactType": "Service Client"
+            }
+          })}
+        </script>
       </Head>
       <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
